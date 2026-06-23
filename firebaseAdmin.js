b9a336f33ffd7e2ae082f748af9baa70930e4ca0
Hassan Babilly <h.babll205@example.com>
2026-06-23 12:29:39 +0300

    use firebase env vars

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,10 +1,12 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
-const serviceAccount = require("./babilly-firebase-adminsdk-fbsvc-5f4aad176b.json");
-
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 module.exports = {
